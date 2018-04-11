eab2f2c8db882e64f11fb3d598a45e1dc0cb1ff1
cristicatalan <catalan.cristi@agilefreaks.com>
2018-04-11 16:16:25 +0300

    [#11] Implement validation

--- lib/dry_twitter/registration/validate.rb
+++ lib/dry_twitter/registration/validate.rb
@@ -1,4 +1,5 @@
 require "dry/transaction/operation"
+require 'dry-validation'
 
 module DryTwitter
   module Registration
@@ -6,7 +7,19 @@ module DryTwitter
       include Dry::Transaction::Operation
 
       def call(input)
-        Failure("not good")
+        schema = Dry::Validation.Schema do
+          required("user_name").filled(:str?, min_size?: 3)
+          required("password").filled(:str?, min_size?: 8)
+          required("confirm_password").filled(:str?, min_size?: 8)
+        end
+
+        result = schema.call(input["user"])
+
+        if (result.success?)
+          Success(input)
+        else
+          Failure(result.messages)
+        end
       end
     end
   end
