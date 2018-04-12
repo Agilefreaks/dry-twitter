917ba552ce46ebb8267ff44187f8da604fb0871c
cristicatalan <catalan.cristi@agilefreaks.com>
2018-04-12 14:55:16 +0300

    [#11] Use form validation

--- lib/dry_twitter/registration/validate.rb
+++ lib/dry_twitter/registration/validate.rb
@@ -6,12 +6,12 @@ module DryTwitter
     class Validate
       include Dry::Transaction::Operation
 
-      SCHEMA = Dry::Validation.Schema do
-        required("user_name").filled(:str?, min_size?: 3)
-        required("password").filled(:str?, min_size?: 8)
-        required("confirm_password").filled(:str?, min_size?: 8)
+      SCHEMA = Dry::Validation.Form do
+        required(:user_name).filled(:str?, min_size?: 3)
+        required(:password).filled(:str?, min_size?: 8)
+        required(:confirm_password).filled(:str?, min_size?: 8)
 
-        rule(confirm_password: %w(password confirm_password)) do |password, confirm_password|
+        rule(confirm_password: %i(password confirm_password)) do |password, confirm_password|
           password.eql?(confirm_password)
         end
       end
--- spec/lib/dry_twitter/registration/validate_spec.rb
+++ spec/lib/dry_twitter/registration/validate_spec.rb
@@ -15,47 +15,47 @@ RSpec.describe DryTwitter::Registration::Validate do
   it 'will fail with user name not filled' do
     result = subject.({ 'user' => { 'user_name' => '' } })
 
-    expect(result.value["user_name"]).to be_truthy
-    expect(result.value["user_name"].size).to eq(2)
-    expect(result.value["user_name"]).to include("must be filled")
-    expect(result.value["user_name"]).to include("size cannot be less than 3")
+    expect(result.value[:user_name]).to be_truthy
+    expect(result.value[:user_name].size).to eq(2)
+    expect(result.value[:user_name]).to include("must be filled")
+    expect(result.value[:user_name]).to include("size cannot be less than 3")
   end
 
   it 'will fail with user name length of 2' do
     result = subject.({ 'user' => { 'user_name' => 'Cr' } })
 
-    expect(result.value["user_name"]).to be_truthy
-    expect(result.value["user_name"].size).to eq(1)
-    expect(result.value["user_name"]).to include("size cannot be less than 3")
+    expect(result.value[:user_name]).to be_truthy
+    expect(result.value[:user_name].size).to eq(1)
+    expect(result.value[:user_name]).to include("size cannot be less than 3")
   end
 
   it 'will not fail with user name filled correctly' do
     result = subject.({ 'user' => { 'user_name' => 'Cristi' } })
 
-    expect(result.value["user_name"]).to be_falsey
+    expect(result.value[:user_name]).to be_falsey
   end
 
   it 'will fail with password not filled' do
     result = subject.({ 'user' => { 'password' => '' } })
 
-    expect(result.value["password"]).to be_truthy
-    expect(result.value["password"].size).to eq(2)
-    expect(result.value["password"]).to include("must be filled")
-    expect(result.value["password"]).to include("size cannot be less than 8")
+    expect(result.value[:password]).to be_truthy
+    expect(result.value[:password].size).to eq(2)
+    expect(result.value[:password]).to include("must be filled")
+    expect(result.value[:password]).to include("size cannot be less than 8")
   end
 
   it 'will fail with password length of 2' do
     result = subject.({ 'user' => { 'password' => 'pass' } })
 
-    expect(result.value["password"]).to be_truthy
-    expect(result.value["password"].size).to eq(1)
-    expect(result.value["password"]).to include("size cannot be less than 8")
+    expect(result.value[:password]).to be_truthy
+    expect(result.value[:password].size).to eq(1)
+    expect(result.value[:password]).to include("size cannot be less than 8")
   end
 
   it 'will not fail with password filled corrctly' do
     result = subject.({ 'user' => { 'password' => 'password' } })
 
-    expect(result.value["password"]).to be_falsey
+    expect(result.value[:password]).to be_falsey
   end
 
   it 'will fail with password and confirm password different' do
