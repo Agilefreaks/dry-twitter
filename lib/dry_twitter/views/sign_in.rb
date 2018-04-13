1a04f014734ec584f575f40699502fdf05a19e8d
cristicatalan <catalan.cristi@agilefreaks.com>
2018-04-13 15:19:59 +0300

    [#15] Add sign in form

---
+++ lib/dry_twitter/views/sign_in.rb
@@ -0,0 +1,22 @@
+require "dry_twitter/view/controller"
+
+module DryTwitter
+  module Views
+    class SignIn < DryTwitter::View::Controller
+      configure do |config|
+        config.template = "sign_in"
+      end
+    end
+
+    def locals(options = {})
+      if options.size > 0
+        super.merge(
+            user: options[:input]["user"],
+            errors: options[:input][:errors]
+        )
+      else
+        super.merge(user: nil, errors: nil)
+      end
+    end
+  end
+end
