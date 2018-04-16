b790f702eafb20645244a7334cd77bb8db7b26ba
cristicatalan <catalan.cristi@agilefreaks.com>
2018-04-16 17:46:40 +0300

    [#13] Use Try monad for exception handling

--- lib/dry_twitter/registration/persist.rb
+++ lib/dry_twitter/registration/persist.rb
@@ -1,19 +1,16 @@
 require "dry/transaction/operation"
 require "dry_twitter/import"
+require "dry-monads"
 
 module DryTwitter
   module Registration
     class Persist
       include Dry::Transaction::Operation
       include DryTwitter::Import["repositories.users"]
+      include Dry::Monads::Try::Mixin
 
       def call(input)
-        begin
-          users.create(user_name: input["user"]["user_name"], password: input["user"]["password"])
-          Success(input)
-        rescue Exception
-          Failure("An unexpected exception was raised while creating user database record")
-        end
+        Try() {users.create(user_name: input["user"]["user_name"], password: input["user"]["password"])}.to_result
       end
     end
   end
